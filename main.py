720e831ce97299636e10f694d96f4378be7c4be0
Ashley Deshields <ashley.deshields.2016@gmail.com>
2017-10-28 15:29:47 -0400

    Trying to pull a sneaky sneaky

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import render_template
 from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
+import key.py
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,9 +15,9 @@ if __name__ == "__main__":
 
 
 # Your Account SID from twilio.com/console
-account_sid = "AC0dba325e11f49a8384fe1d9cf7e237e5"
+account_sid = MY_ID
 # Your Auth Token from twilio.com/console
-auth_token  = "1c1002ab99bb77ca50c9adb69a121683"
+auth_token  = AUTH_KEY
 
 client = Client(account_sid, auth_token)
 
