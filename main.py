be134981cd417faecb9d32d3762c891f40d60143
Ashley Deshields <ashley.deshields.2016@gmail.com>
2017-10-28 16:48:46 -0400

    Mistakes were made but that's okay

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     app.run(debug=true)
 
 
-
 # Your Account SID from twilio.com/console
 account_sid = MY_ID
 # Your Auth Token from twilio.com/console
@@ -21,12 +20,35 @@ auth_token  = AUTH_KEY
 
 client = Client(account_sid, auth_token)
 
-def sendSMSFromBot(text, user):
+bot_num = "+16672399678"
+
+# Some global variables
+age = 0
+female = false
+male = false
+symptoms = []
+
+
+def sendSMS(text, user): #LOL dont need it Ashley will fix
     message = client.messages.create(
         to=user,
-        from_= "+16672399678",
+        from_= bot_num,
         body=text)
     print(message.sid)
     return
 
-def
+def background_check(user):
+    sendSMS("Hello. First, we must do a quick background check.", user)
+    sendSMS("Are you biologically male or female?", user)
+    gender_wait = True
+    while gender_wait:
+        if input().lowercase == "female":
+            female = true
+            gender_wait = False
+
+        elif input().lowercase == "male":
+            male = true
+            gender_wait = False
+
+        else:
+            sendSMS("Please text \"male\" or \"female\.")
