0e6e1efe6086b86315af185b78088d00d0b13c32
ar <arod1198@gmail.com>
2017-10-29 07:48:32 -0400

    Finished files for our incomplete project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from flask import Flask, request, session
-from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.messaging_response import MessagingResponse, Message
 from twilio.rest import Client
 from key import MY_ID, AUTH_KEY
 
@@ -23,12 +23,18 @@ male = False
 symptoms = []
 dicnumb = {}
 
-def sendSMS(text, user): #LOL dont need it Ashley will fix
+def sendSMS(text, user): 
     message = client.messages.create(
         to=user,
         from_= bot_num,
         body=text)
     print(message.sid)
+
+# def receiveSMS():
+#     message = client.messages() \
+#                 .fetch()
+#     print(message.body.encode('utf-8'))
+#     return message
 
 def check_in_dicnumb(number, message):
     global dicnumb
@@ -38,54 +44,71 @@ def check_in_dicnumb(number, message):
         dicnumb[number].append(message)
 
 @app.route("/", methods=['GET', 'POST'])
-def background_check():
+def fullBackgroundCheck():
     counter = session.get('counter', 0)
     counter += 1
     session['counter'] = counter
     fr_num = request.values.get('From', None) # get_number
     init_resp = request.values.get("Body", None) # get_firstmessage
-    check_in_dicnumb(fr_num, init_resp) # checks if number is in dict
+    check_in_dicnumb(fr_num, init_resp)
     sendSMS("Hello. First, we must do a quick background check.", fr_num)
+    
+    #Function 1: Bio gender
+    gender_check()
 
-    print(str(init_resp))
+    #Function 2: Year
+    birth_check()    
+
+
+def gender_check():
+    fr_num = request.values.get('From', None) # get_number
+    # init_resp = request.values.get("Body", None) # get_firstmessage
+    # check_in_dicnumb(fr_num, init_resp) # checks if number is in dict
+    # sendSMS("Hello. First, we must do a quick background check.", fr_num)
+
+    # print(str(init_resp))
     # resp = MessagingResponse()
 
     sendSMS("Are you biologically male or female?", fr_num)
     
-
     gender_wait = True
     while gender_wait:
-        resp = MessagingResponse()
+        respa = MessagingResponse()
+        respa.message()
+        print(str(respa) + '\n')
         gender_resp = request.values.get('Body', None)
-        if gender_resp.lower() == "female":
+        return
+        if gender_resp.split(" ")[0].lower() == "female":
             female = True
             gender_wait = False
 
-        elif gender_resp.lower() == "male":
+        elif gender_resp.split(" ")[0].lower() == "male":
             male = True
             gender_wait = False
 
         else:
-            
             sendSMS("Please text \"male\" or \"female.\"", fr_num)
-            print(str(resp))
-            gender_wait = False
+            print(str(resp) + '\n')
+    return 
 
-    resp = MessagingResponse()
+
+def birth_check():
+    fr_num = request.values.get('From', None) # get_number
+    init_resp = request.values.get("Body", None) # get_firstmessage
+    
     sendSMS("What year were you born?", fr_num)
-    print(str(resp))
-
     year_wait = True
     while year_wait:
         resp = MessagingResponse()
-        year_resp = request.values.get('Body', None)
-        if int(year_resp) > 1900:
+        year_resp = request.values.get('Body', None) #breaks cuz it takes the body of first input
+        if int(year_resp) < 1900:
             year = int(year_resp)
             year_wait = False
 
         else:
             sendSMS("The probability of you being that old is quite low. Please put a more realistic year.", fr_num)
             print(str(resp))
+    return
 
 if __name__ == "__main__":
      app.run(debug=True)
--- textTest.py
+++ textTest.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from twilio.twiml.messaging_response import MessagingResponse, Message
+from twilio.rest import Client
 
 app = Flask(__name__)
 
@@ -8,9 +9,12 @@ def sms_reply():
     """Respond to incoming calls with a simple text message."""
 
     resp = MessagingResponse()
-    msg=resp.message("Here's a meme!")
-    msg.media("https://assets-auto.rbl.ms/13273cc2f3968434cec2e5a95ff8e334d7d2d83e342f1a7bce298921eda06399")
+    
+    msg=resp.message(str(client.messages.list()))
+    #msg.media("https://assets-auto.rbl.ms/13273cc2f3968434cec2e5a95ff8e334d7d2d83e342f1a7bce298921eda06399")
     return str(resp)
+
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
